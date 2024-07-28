9f88209692bb3f056bdd1d8b97eff5b6fb018496
Yogi-ArdiansyahN <78415006+Yogi-ArdiansyahN@users.noreply.github.com>
2024-07-28 18:38:07 +0700

    add index profile

--- app/Controllers/Profile.php
+++ app/Controllers/Profile.php
@@ -8,6 +8,28 @@ class Profile extends BaseController
 {
     public function index()
     {
-        //
+
+        $saldo = 100000;
+
+        if ($saldo >= 1000000000) {
+            $saldo = number_format($saldo / 1000000000, 1) . 'B';
+        } elseif ($saldo >= 1000000) {
+            $saldo = number_format($saldo / 1000000, 1) . 'M';
+        } elseif ($saldo >= 1000) {
+            $saldo = number_format($saldo / 1000, 1) . 'K';
+        } else {
+            $saldo = number_format($saldo);
+        }
+
+        $data = [
+            'title' => 'Profile',
+            'active' => 'profile',
+            'username' => 'Ahmad Haris',
+            'saldo' => $saldo,
+            'gabung' => '30 Januari 2029',
+            'email' => 'yogiardiansyahn@gmail.com',
+            'nohp' => '085156511123'
+        ];
+        return view('profile/index', $data);
     }
 }
